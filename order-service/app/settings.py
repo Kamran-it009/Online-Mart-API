69b59cbce596df4daf077b2cc214c40b305a29dd
Muhammad Kamran Khan <109745004+Kamran-it009@users.noreply.github.com>
2024-07-17 18:02:16 +0500

    Add files via upload

---
+++ order-service/app/settings.py
@@ -0,0 +1,19 @@
+from starlette.config import Config
+from starlette.datastructures import Secret
+
+try:
+    config = Config(".env")
+except FileNotFoundError:
+    config = Config()
+
+DATABASE_URL = config("DATABASE_URL", cast=Secret)
+
+KAFKA_ORDER_TOPIC = config("KAFKA_ORDER_TOPIC", cast=str, default = "order-topic")
+
+BOOTSTRAP_SERVER = config("BOOTSTRAP_SERVER", cast=str)
+
+KAFKA_CONSUMER_GROUP_ID_FOR_ORDER = config("KAFKA_CONSUMER_GROUP_ID_FOR_ORDER", cast=str, default="default_group_id")
+
+TEST_DATABASE_URL = config("TEST_DATABASE_URL", cast=Secret)
+
+OPENAI_API_KEY = config("OPENAI_API_KEY", cast=Secret)
